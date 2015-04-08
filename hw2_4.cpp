7feaf1021e136e336f6b6db4285a71ce7dc6b425
cftang0827 <cftang0827@gmail.com>
2015-04-09 02:28:09 +0800

    change string to unsigned long long and change calling method

--- hw2_4.cpp
+++ hw2_4.cpp
@@ -12,7 +12,7 @@
 
 
 using namespace std;
-typedef vector<string> Data;
+typedef vector<unsigned long long > Data;
 typedef vector<Data> DataList;
 string NumOfData;
 string click;
@@ -28,6 +28,21 @@ string TID;
 string DID ;
 string UID;
 string a ;
+
+unsigned long long NumOfData1;
+unsigned long long click1;
+unsigned long long impression1;
+unsigned long long URL1;
+unsigned long long AdID1;
+unsigned long long AdvertiserID1;
+unsigned long long depth1;
+unsigned long long position1;
+unsigned long long QID1;
+unsigned long long KID1 ;
+unsigned long long TID1;
+unsigned long long DID1 ;
+unsigned long long UID1;
+
 
 DataList *list1 = new  DataList[tableSize];	
 
@@ -53,146 +68,95 @@ int main(int argc,char* argv[])
         //exit(1);
     }
 
-
-
-    //	ofstream out1(argv[2],ios::out);
-
-    //	if(!out1)
-    //	{
-    //		cerr<<"Output File error"<<endl;
-    //exit(1);
-    //	}
-
-    //	out1 << "Click"<<'\t'<<"impression"<<'\t'<<"URL"<<'\t'<<'\t'<<"AdID"<<'\t'<<"AdvertiserID"<<'\t'<<"depth"<<'\t'<<"position"<<'\t'<<"QID"<<'\t'<<"KID"<<'\t'<<"TID"<<'\t'<<"DID"<<'\t'<<"UID"<<endl;
-
     int count = 0;
-    while(in1 >>click>>impression>>URL>>AdID>>AdvertiserID>>depth>>position>>QID>>KID>>TID>>DID>>UID)
+    while(in1 >>click1>>impression1>>URL1>>AdID1>>AdvertiserID1>>depth1>>position1>>QID1>>KID1>>TID1>>DID1>>UID1)
     {
-        stringstream ss;
         Data data1;
-
-        //		out1 << click[count]<<'\t'<<impression[count]<<'\t'<<URL[count]<<'\t'<<AdID[count]<<'\t'<<AdvertiserID[count]<<'\t'<<depth[count]<<'\t'<<position[count]<<'\t'<<QID[count]<<'\t'<<KID[count]<<'\t'<<TID[count]<<'\t'<<DID[count]<<'\t'<<UID[count]<<endl;
-        //out1 << click[count]<<'\t'<<impression[count]<<endl;
-
-        data1.push_back(click);
-        data1.push_back(impression);
-        data1.push_back(URL);
-        data1.push_back(AdID);
-        data1.push_back(AdvertiserID);
-        data1.push_back(depth);
-        data1.push_back(position);
-        data1.push_back(QID);
-        data1.push_back(KID);
-        data1.push_back(TID);
-        data1.push_back(DID);
-        data1.push_back(UID);
-
-        /*
-           for(int i=0;i<12;i++)
-           {
-           cout<<data1[i]<<" ";
-           }
-           cout<<endl;
-         */
-        ss<<UID;
-        int temp;
-        ss>>temp;
-        //cout<<"temp"<<temp<<endl;
-        //cout<<"count:"<<count<<endl;
+/*
+		stringstream a1, a2, a3, a4, a5, a6, a7,a8,a9,a10,a11,a12;
+		a1<<click;
+		a2<<impression;
+		a3<<URL;
+		a4<<AdID;
+		a5<<AdvertiserID;
+		a6<<depth;
+		a7<<position;
+		a8<<QID;
+		a9<<KID;
+		a10<<TID;
+		a11<<DID;
+		a12<<UID;
+		
+		a1>>click1;
+		a2>>impression1;
+		a3>>URL1;
+		a4>>AdID1;
+		a5>>AdvertiserID1;
+		a6>>depth1;
+		a7>>position1;
+		a8>>QID1;
+		a9>>KID1;
+		a10>>TID1;
+		a11>>DID1;
+		a12>>UID1;
+		
+	*/
+        data1.push_back(click1);
+        data1.push_back(impression1);
+        data1.push_back(URL1);
+        data1.push_back(AdID1);
+        data1.push_back(AdvertiserID1);
+        data1.push_back(depth1);
+        data1.push_back(position1);
+        data1.push_back(QID1);
+        data1.push_back(KID1);
+        data1.push_back(TID1);
+        data1.push_back(DID1);
+        data1.push_back(UID1);
+		
+        int temp = UID1;
+        
         int index;
         index = temp%tableSize ;
-        //cout<<"index"<<index<<endl;
+        
         list1[index].push_back(data1);
-        //		system("PAUSE");
-
-        //cout<<"temp:"<<temp<<endl;
-
-        //cout<<"temp%10="<<temp%10<<endl;
-        //cout<<"count:"<<count<<endl;
-
-
-
-
-
-        /*
-           switch(temp%10)
-           {
-
-           case 1:
-           a1.push_back(data1);
-           break;	
-           case 2:
-           a2.push_back(data1);
-           break;	
-           case 3:
-           a3.push_back(data1);
-           break;
-           case 4:
-           a4.push_back(data1);
-           break;	
-           case 5:
-           a5.push_back(data1);
-           cout<<"FUXK"<<endl;
-           break;
-           case 6:
-           a6.push_back(data1);
-           cout<<"FUXK666"<<endl;
-           break;	
-           case 7:
-           a7.push_back(data1);
-           break;	
-           case 8:
-           a8.push_back(data1);
-           break;	
-           case 9:
-           a9.push_back(data1);
-           break;	
-           default:
-           a10.push_back(data1);
-           }
-         */
-        //data1.clear();
-        //cout<<"aaaaaaaaaaaaaaaaaaa"<<endl;
-
+	
         count++;
-        //datalist.push_back(data);
+       
         if(count == inputSize)
             break;
 
     }
 
-    //vector<DataList>::iterator iter = list[4].begin();
-    /*
-       cout<<"list1[0]size=="<<list1[0].size()<<endl;
-       cout<<"list1[1]size=="<<list1[1].size()<<endl;
-       cout<<"list1[2]size=="<<list1[2].size()<<endl;
-       cout<<"list1[3]size=="<<list1[3].size()<<endl;
-       cout<<"list1[4]size=="<<list1[4].size()<<endl;
-       cout<<"list1[5]size=="<<list1[5].size()<<endl;
-       cout<<"list1[6]size=="<<list1[6].size()<<endl;
-       cout<<"list1[7]size=="<<list1[7].size()<<endl;
-       cout<<"list1[8]size=="<<list1[8].size()<<endl;
-       cout<<"list1[9]size=="<<list1[9].size()<<endl;
-       for(int i=0;i<12;i++)
-       cout<<"list1[4][5][i]:"<<list1[4][5][i]<<endl;
-     */
-    /*
-       for(int i=0;i<list1[5].size();i++)
-       {
-       for(int j=0;j<12;j++)
-       {
-       cout<<list1[5][i][j]<<" ";
-       }
-       cout<<endl;
-       }*/
-
-    //system("PAUSE");
 	t1 = t1-clock();
 	printf ("It took me %d clicks in making data sheet(%f seconds).\n",t1,((float)t1)/CLOCKS_PER_SEC);
-
-   get(490234,21560664,2255103,2,2);
-    //clicked(490234);
+	string order;
+	unsigned long long get_in1,get_in2,get_in3,get_in4,get_in5;
+	unsigned long long click_in; 
+	while(cin>>order)
+	{
+		if(order == "get")
+		{
+			cin>>get_in1>>get_in2>>get_in3>>get_in4>>get_in5;
+			get(get_in1,get_in2,get_in3,get_in4,get_in5);
+		}else if(order == "clicked")
+		{			
+			
+			
+			cin>>click_in;
+			clicked(click_in);
+			
+		}		
+		
+		
+	}
+	
+	
+	
+    //get(490234,21560664,2255103,2,2);
+	//clicked(490234);
 	//impressed(6231944,490234);
+   
 	
     t = t-clock();
     printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
@@ -205,102 +169,45 @@ int count1=0;
 
 void get(int u,int a,int q,int p,int d)
 {
-    cout<<"we r doing get function!!!!!!!!!!!!!!!!!!!!!!"<<endl;
+    
     int click_out=0;
     int imp_out=0;
-    stringstream cs;
-    stringstream is;
-    stringstream us;
-    stringstream as;
-    stringstream qs;
-    stringstream ds;
-    stringstream ps;
-    string ui;
-    string di;
-    string ai;
-    string pi;
-    string qi;
-
-    int ci;
-    int ii;
-
-    as<<a;
-    qs<<q;
-    ps<<p;
-    ds<<d;
-    us<<u;
-    as>>ai;
-    qs>>qi;
-    ps>>pi;
-    ds>>di;
-    us>>ui;
+	
     int index1 = u%tableSize;
-    /*
-       cout<<"index1="<<index1<<endl;
-       cout<<"ai"<<ai<<endl;
-       cout<<"qi"<<qi<<endl;
-       cout<<"pi"<<pi<<endl;
-       cout<<"di"<<di<<endl;
-       cout<<"ui"<<ui<<endl;
-     */
+    
     for(int i=0;i<list1[index1].size();i++)
     {
-        /*
-           cout<<"list1[index1][i][3]AdID="<<list1[index1][i][3]<<endl;
-           cout<<"list1[index1][i][7]QID="<<list1[index1][i][7]<<endl;
-           cout<<"list1[index1][i][5]Depth="<<list1[index1][i][5]<<endl;
-           cout<<"list1[index1][i][6]Position="<<list1[index1][i][6]<<endl;
-           cout<<"i:::"<<i<<endl;
-         */
-        if( (list1[index1][i][3] == ai) && (list1[index1][i][7] == qi) && (list1[index1][i][5] == di) &&(list1[index1][i][6] == pi) &&(list1[index1][i][11] ==ui) )
-        {
-
-            cs<<list1[index1][i][0];
-            is<<list1[index1][i][1];
-            cs>>ci;
-            is>>ii;
-            cs.clear();
-            is.clear();
-            //cout<<"ci:"<<ci<<endl;
-            //cout<<"list[index1][i][0]:"<<list1[index1][i][0]<<endl;
-            //cout<<"ci::"<<ci<<endl;
-            click_out = click_out+ci;
-            imp_out = imp_out +ii;
-
-            //cout<<"get("<<u<<","<<a<<","<<q<<","<<p<<","<<d<<")=";
-            //cout<<"("<<ci<<", "<<ii<<")"<<endl;
+        
+        if( (list1[index1][i][3] == a) && (list1[index1][i][7] == q) && (list1[index1][i][5] == d) &&(list1[index1][i][6] == p) &&(list1[index1][i][11] ==u) )
+        { 
+			click_out = click_out +list1[index1][i][0];
+			imp_out = imp_out +list1[index1][i][1];
+            
         }
 
     }
     cout<<"click_time:"<<click_out<<endl;
     cout<<"impression_time"<<imp_out<<endl;
-    cout<<endl;
+    //cout<<endl;
 
 }
 
 void clicked(int u)
 {
-    //Data temp;
+    
     DataList temp_array;
-    stringstream ss;
-    string us;
-    ss<<u;
-    ss>>us;
+   
     int index2 = u%tableSize;
-    int pter;
-    stringstream rr;
+    unsigned long pter;
+    
     for(int i=0;i<list1[index2].size();i++)
     {
-        rr.clear();
-        rr<<list1[index2][i][0];
-        rr>>pter;
+        
+		pter = list1[index2][i][0];
 
-        if(list1[index2][i][11] == us && pter>=1 &&replicate(list1[index2],i ))
+        if(list1[index2][i][11] == u && pter>=1 &&replicate(list1[index2],i ))
         {
-            //cout<<"clicked("<<u<<")=";
-            //cout<<"("<<list1[index2][i][3]<<", "<<list1[index2][i][7]<<")"<<endl;
-
-
+            
             Data temp;
             temp.push_back(list1[index2][i][3]);
             temp.push_back(list1[index2][i][7]);
@@ -319,7 +226,7 @@ void clicked(int u)
         cout<<endl;
     }
 
-	cout<<endl;
+	//cout<<endl;
 }
 
 
@@ -349,53 +256,46 @@ void impressed(int u1,int u2)
 	
 	int index3 = u1%tableSize;
 	int index4 = u2%tableSize;
-	stringstream ss1;
-	stringstream ss2;
-	string us1;
-	string us2;
-	ss1<<u1;
-	ss2<<u2;
-	ss1>>us1;
-	ss2>>us2;
+	
 	DataList temp1;
 	DataList temp2;
-	map<string,DataList> container;
-	//map<string,DataList> container2;
+	map<unsigned long,DataList> container;
+	
 	
 	for(int i=0;i<list1[index3].size();i++)
 	{
-		if(list1[index3][i][11] == us1 )//&& list1[index3][i][0]!="0")
+		if(list1[index3][i][11] == u1 )//&& list1[index3][i][0]!="0")
 		{
 			temp1.push_back(list1[index3][i]);
 		}
-		cout<<temp1[i][3]<<" ";
-		//DataList t1;
-		if(temp1[i][1] != "0")
+		//cout<<temp1[i][3]<<" ";
+		
+		if(temp1[i][1] != 0)
 		container[temp1[i][3]].push_back(temp1[i]);
 	}
-	cout<<endl;
+	//cout<<endl;
 	for(int j=0;j<list1[index4].size();j++)
 	{
-		if(list1[index4][j][11] == us2 )//&&list1[index4][j][0]!="0")
+		if(list1[index4][j][11] == u2 )//&&list1[index4][j][0]!="0")
 		{
 			temp2.push_back(list1[index4][j]);
 		}
-		cout<<temp2[j][3]<<" ";
-		if(temp2[j][1] != "0")
+		//cout<<temp2[j][3]<<" ";
+		if(temp2[j][1] != 0)
 		container[temp2[j][3]].push_back(temp2[j]);
 	}
 	cout<<endl;
 	
 	//cout<<container[temp2[0][3]][0][3]<<endl;
 	vector<DataList> con;
-	for(map<string,DataList>::iterator it=container.begin();it!=container.end(); ++it)
+	for(map<unsigned long,DataList>::iterator it=container.begin();it!=container.end(); ++it)
 	{
 		
-		cout<<it->first<<": "<<endl;
+		//cout<<it->first<<": "<<endl;
 		for(int u=0;u<(it->second).size();u++)
 		{
-			cout<<(it->second)[u][2]<<" "<<(it->second)[u][4]<<" "<<(it->second)[u][8]<<" "<<(it->second)[u][9]<<" "<<(it->second)[u][10]<<" "<<(it->second)[u][11]<<" ";
-			cout<<endl;
+			//cout<<(it->second)[u][2]<<" "<<(it->second)[u][4]<<" "<<(it->second)[u][8]<<" "<<(it->second)[u][9]<<" "<<(it->second)[u][10]<<" "<<(it->second)[u][11]<<" ";
+			//cout<<endl;
 			if((it->second).size()!=1 )
 			{
 				if((it->second)[u][11] != (it->second)[u+1][11])
